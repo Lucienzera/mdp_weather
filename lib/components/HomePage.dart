79bb8e76670a5c6b1792fdbee3843b634092c4f6
Lucien Zera <lucienzera26@gmail.com>
2019-08-04 17:26:35 +0800

    Update Homepage.dart
    Added searchbar and wrapped the whole thing with listview to avoid error overflow

--- lib/components/HomePage.dart
+++ lib/components/HomePage.dart
@@ -12,54 +12,68 @@ class _HomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
+        body: ListView(
       children: <Widget>[
-        Stack(
+        Column(
           children: <Widget>[
-            CurvedBackground(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+            Stack(
               children: <Widget>[
+                CurvedBackground(),
                 Container(
-                  padding: EdgeInsets.all(40.0),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.more_vert,
-                      size: 40.0,
-                      color: Colors.white,
+                    color: Colors.transparent,
+                    padding: EdgeInsets.all(10.0),
+                    margin: EdgeInsets.fromLTRB(40.0, 100, 40.0, 0.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Color.fromRGBO(255, 255, 255, 0.6),
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(10.0))),
+                      child: TextField(
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.w300),
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Colors.white,
+                            size: 34.0,
+                          ),
+                          hintText: "Search",
+                          hintStyle: TextStyle(color: Colors.grey),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.transparent),
+                          ),
+                        ),
+                      ),
+                    )),
+                Padding(
+                  padding: EdgeInsets.only(top: 200.0),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          'Sunny',
+                          style: TextStyle(fontSize: 26.0, color: Colors.white),
+                        ),
+                        Text(
+                          '26',
+                          style: TextStyle(
+                              fontSize: 100.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        Text(
+                          'Subang Jaya, Selangor',
+                          style: TextStyle(fontSize: 20.0, color: Colors.white),
+                        )
+                      ],
                     ),
                   ),
-                ),
+                )
               ],
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 200.0),
-              child: Align(
-                alignment: Alignment.center,
-                child: Column(
-                  children: <Widget>[
-                    Text(
-                      'Sunny',
-                      style: TextStyle(fontSize: 26.0, color: Colors.white),
-                    ),
-                    Text(
-                      '26',
-                      style: TextStyle(
-                          fontSize: 100.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    ),
-                    Text(
-                      'Subang Jaya, Selangor',
-                      style: TextStyle(fontSize: 20.0, color: Colors.white),
-                    )
-                  ],
-                ),
-              ),
             )
           ],
-        )
+        ),
       ],
     ));
   }
